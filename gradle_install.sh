6946097534ac108745c49392e47a5da2c2f1d308
le2sky <le2sky@kakao.com>
2022-07-08 02:50:34 +0000

    src: modiried gradle_install.sh

--- gradle_install.sh
+++ gradle_install.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-gradle_version=”4.4-rc-5″
 
-#Your Custom Installation Path
+# apt-get update
+sudo apt-get update
+sudo apt-get -y upgradle
+sudo apt-get install build-essential
+sudo apt-get install curl
 
-install_target_path=”/opt/gradle”
-wget -N https://services.gradle.org/distributions/gradle-${gradle_version}-all.zip
-mkdir -p ${install_target_path}
-unzip gradle-${gradle_version}-all.zip
-mv gradle-${gradle_version} ${install_target_path}/
-ln -sfn gradle-${gradle_version} ${install_target_path}/latest
-chown -R ec2-user:ec2-user ${install_target_path}
-printf “export GRADLE_HOME=${install_target_path}/latest\nexport PATH=\$PATH:\$GRADLE_HOME/bin” > /etc/profile.d/gradle.sh
-. /etc/profile.d/gradle.sh
+# gradle installation
+sudo apt-get install gradle-7.4.2
+
 # check installation
 gradle -v
