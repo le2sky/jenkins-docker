32759302a44afaac7431c1ef133e9f1a62f7751c
leesky <leehaneul0623@gmail.com>
2022-07-10 16:54:33 +0900

    fix: fix gradle_latest intsallation script

--- gradle_install.sh
+++ gradle_install.sh
@@ -6,8 +6,9 @@ sudo apt-get update
 sudo apt-get -y upgradle
 sudo apt-get install unzip
 sudo apt-get install curl
-sudo apt-get install gedit
 
+# jdk install
+sudo apt-get install openjdk-11-jdk
 
 # gradle version setting
 VERSION=7.4.2
@@ -20,17 +21,18 @@ sudo unzip -d /opt/gradle /tmp/gradle-${VERSION}-bin.zip
 sudo ln -s /opt/gradle/gradle-${VERSION} /opt/gradle/latest
 
 
-# sudo gedit /etc/profile.d/gradle.sh
+# sudo vi /etc/profile.d/gradle.sh
 
-# input flowwing command into gradle.sh
+## input flowwing command into gradle.sh
 # export GRADLE_HOME=/opt/gradle/latest
 # export PATH=${GRADLE_HOME}/bin:${PATH}
 
-# add execuatable permission
+## add execuatable permission
 # sudo chmod +x /etc/profile.d/gradle.sh
 
-#  script loading
+##  script loading
 # source /etc/profile.d/gradle.sh
 
 # check installation
 gradle -v
+
